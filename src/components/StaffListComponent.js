b93f2ae06d14f28dc6c232443985b795b86f49e9
Long <longnmfx16757@funix.edu.vn>
2022-06-26 19:17:06 +0700

    render staff on click

--- src/components/StaffListComponent.js
+++ src/components/StaffListComponent.js
@@ -5,12 +5,36 @@ import dateFormat from "dateformat";
 class StaffList extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = { selectedStaff: null };
   }
+
+  onStaffSelect(staff) {
+    this.setState({ selectedStaff: staff });
+  }
+
+  renderStaffSelect(staff) {
+    if (staff != null) {
+      return (
+        <Card>
+          <CardBody>
+            <CardTitle>Họ và tên:{staff.name}</CardTitle>
+            <CardText>Ngày sinh:{dateFormat(staff.doB, "dd/mm/yyyy")}</CardText>
+            <CardText>
+              Ngày vào công ty:{dateFormat(staff.startDate, "dd/mm/yyyy")}
+            </CardText>
+            <CardText>Phòng ban:{staff.department.name}</CardText>
+            <CardText>Số ngày nghỉ còn lại:{staff.annualLeave}</CardText>
+            <CardText>Số ngày đã làm thêm:{staff.overTime}</CardText>
+          </CardBody>
+        </Card>
+      );
+    } else return <p>Bấm vào tên nhân viên để xem thông tin.</p>;
+  }
+
   render() {
     const staffs = this.props.staffs.map((staff) => {
       return (
-        <div className="col-12 col-md-6 col-lg-4">
+        <div className="col-12 col-md-6 col-lg-4 p-1">
           <Card key={staff.id} onClick={() => this.onStaffSelect(staff)}>
             <CardTitle>{staff.name}</CardTitle>
           </Card>
@@ -20,6 +44,11 @@ class StaffList extends Component {
     return (
       <div className="container">
         <div className="row">{staffs}</div>
+        <div className="row">
+          <div className="col-12 col-md-6 col-lg-4 p-1">
+            {this.renderStaffSelect(this.state.selectedStaff)}
+          </div>
+        </div>
       </div>
     );
   }
